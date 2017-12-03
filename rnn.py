de17640972000b634a01863f5cab3d43e092a92a
Shruthi Yashavanth <syashavanth@umass.edu>
2017-12-02 19:11:31 -0500

    rnn headache

--- rnn.py
+++ rnn.py
@@ -36,6 +36,7 @@ for i in range(N):
 x = Variable(torch.from_numpy(vec), requires_grad=False)
 y = Variable(torch.from_numpy(label_arr).type(dtype), requires_grad=False)
 
+criterion = nn.NLLLoss()
 
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -61,11 +62,71 @@ n_hidden = 128
 rnn = RNN(D_in, H, D_out)
 
 vec_reshaped = vec.reshape(N,1,D_in)
-input = Variable(torch.from_numpy(vec_reshaped))
+inp = Variable(torch.from_numpy(vec_reshaped),  requires_grad=False)
 
-hidden = Variable(torch.zeros(1, H))
-output, next_hidden = rnn(input[0],hidden)
+hidden = Variable(torch.zeros(1, H), requires_grad = True)
+output, next_hidden = rnn(inp[0],hidden)
 
 pred_array = output.data.numpy()
 pred = pred_array.argmax(axis=1)
 
+#########TRAINING################
+
+def oneTrainingExample(i):
+    category = int(fpdata[i])
+    line = vec[i].reshape(1,D_in)
+    category_tensor = Variable(torch.LongTensor([category]), requires_grad = False)
+    line_tensor = Variable(torch.from_numpy(line), requires_grad = False)
+    return category, line, category_tensor, line_tensor
+
+
+
+learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+
+    #rnn.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor, hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    # Add parameters' gradients to their values, multiplied by learning rate
+    for p in rnn.parameters():
+        p.data.add_(-learning_rate, p.grad.data)
+
+    return output, loss.data[0]
+
+current_loss = 0
+all_losses = []
+n_iters = 100
+#
+#def categoryFromOutput(output):
+#    top_n, top_i = output.data.topk(1) # Tensor out of Variable with .data
+#    category_i = top_i[0][0]
+#    return all_categories[category_i], category_i
+
+
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = oneTrainingExample(iter)
+    
+    #category_tensor = n
+    #line_tensor = inp[0]
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    # Print iter number, loss, name and guess
+    if iter % 5 == 0:
+        top_n, top_i = output.data.topk(3)
+        if category in top_i:
+            print "yes"
+        #print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+
+    # Add current loss avg to list of losses
+    #if iter % plot_every == 0:
+    #    all_losses.append(current_loss / plot_every)
+    #    current_loss = 0
+    break;
